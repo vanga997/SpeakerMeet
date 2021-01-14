d67e83c560017855215b5d807cb3cc25af0ccfcb
vanga997 <promtman1990@gmail.com>
2021-01-14 17:00:27 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# SpeakerMeet" 
---
+++ SpeakerMeet.API/Controllers/SpeakerController.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+namespace SpeakerMeet.API.Controllers
+{
+	[Route("api/[controller]")]
+	public class SpeakerController : Controller
+	{
+	}
+}
